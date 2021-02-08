4aa2d3a397106b017dd99259e1516b0eef60da52
Oliver Singh <ollisingh@hotmail.co.uk>
2021-02-09 00:59:38 +0100

    efficiency change

--- invariants.py
+++ invariants.py
@@ -1,10 +1,13 @@
 import sympy
-from sympy import PurePoly, eye, pexquo, SparseMatrix, zeros
+from sympy import PurePoly, eye, pexquo, zeros, SparseMatrix
 from sympy.abc import t
+
+import sys
+import os
 
 sys.path.append(os.path.dirname(__file__))
 
-
+import sparsedet
 
 def _homology_generators(braid):
     hom_generators = [0]*(len(braid)-1)
@@ -20,7 +23,7 @@ def _homology_generators(braid):
 
 def seifert_matrix(braid):
     hom_generators = _homology_generators(braid)
-    s_matrix = zeros(len(hom_generators), len(hom_generators))
+    s_matrix = sparsedet.SparseMatrix.zeros(len(hom_generators), len(hom_generators))
 
     for i in range(len(hom_generators)):
         if hom_generators[i] == 0:
@@ -38,7 +41,7 @@ def seifert_matrix(braid):
                 else:
                     s_matrix[i, j] = -1
 
-            if i < j < hom_generators[i] < hom_generators[j]:
+            elif i < j < hom_generators[i] < hom_generators[j]:
                 separating_strands = abs(braid[i]) - abs(braid[j])
                 if separating_strands == 1:
                     s_matrix[j, i] = -1
@@ -47,8 +50,8 @@ def seifert_matrix(braid):
 
     for i in range(len(hom_generators) - 1, 0, -1):
         if hom_generators[i] == 0:
-            s_matrix.row_del(i)
-            s_matrix.col_del(i)
+            s_matrix = s_matrix.row_del(i)
+            s_matrix = s_matrix.col_del(i)
     return s_matrix
 
 def _get_reduced_burau_matrices(n):
@@ -90,14 +93,12 @@ def _normalise_laurent(polynomial, symbol):
     :return: normalised polynomial, sympy Poly or PurePoly
     """
     polynomial = polynomial.as_poly(t, 1/t)
-    assert set(polynomial.gens).issubset({symbol,1/symbol}), f"polynomial is not a laurent polynomial in {symbol}"
-    if 1/symbol in polynomial.gens:
-        negative_degree = polynomial.degree(1/symbol)
-        polynomial = (polynomial.as_expr() * symbol**negative_degree).as_poly(symbol)
-
-    elif polynomial.gens == (symbol):
+    negative_degree = polynomial.degree(1/symbol)
+    polynomial = (polynomial.as_expr() * symbol**negative_degree).as_poly(symbol)
+    if negative_degree == 0:
         min_degree = min([mono[0] for mono in polynomial.monoms()])
         polynomial = (polynomial.as_expr() * symbol**(-min_degree)).as_poly(symbol)
+
     if polynomial.nth(0) < 0:
         polynomial = - polynomial
     return polynomial
@@ -116,6 +117,45 @@ def burau_to_alexander(matrix):
     if alex_poly.nth(0) < 0:
         alex_poly = - alex_poly
     return alex_poly
+
+
+def decompose_poly_matrix(poly_matrix):
+    poly_matrix = sympy.Matrix(poly_matrix)
+    rows = poly_matrix.rows
+    cols = poly_matrix.cols
+    matrix_dict = {}
+
+    for row in range(rows):
+        for col in range(cols):
+            poly = sympy.PurePoly(poly_matrix[row,col],t,1/t)
+            for degrees, coeff in zip(poly.monoms(), poly.coeffs()):
+                assert degrees[0] == 0 or degrees[1] == 0, "mixed expression!!"
+                if degrees[0] > 0:
+                    degree = degrees[0]
+                elif degrees[1] > 0:
+                    degree = - degrees[1]
+                else:
+                    degree = 0
+                if not degree in matrix_dict.keys():
+                    matrix_dict[degree] = zeros(rows, cols)
+
+                matrix_dict[degree][row, col] = coeff
+    return matrix_dict
+
+def min_max_deg_degrees(poly_matrix):
+    matrix_dict = decompose_poly_matrix(poly_matrix)
+    degrees = sorted(matrix_dict.keys())
+    for degree in degrees:
+        det = matrix_dict[degree].det()
+        if det != 0:
+            min_degree = degree
+            break
+    for degree in degrees[::-1]:
+        det = matrix_dict[degree].det()
+        if det != 0:
+            max_degree = degree
+            break
+    return min_degree, max_degree
 
 
 def seifert_to_alexander(seifert_matrix):
